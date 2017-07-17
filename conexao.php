efcd3fb9b54d83b2ec09114b0517892034ca270b
Davi Santos <davi.santos14@fatec.sp.gov.br>
2017-07-16 21:57:16 -0300

    Classes de conexões alterados para localhost

--- conexao.php
+++ conexao.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conexao = mysqli_connect("webachei.com.br", "webachei_admin", "@ZBweee6Iw?M", "webachei_bd");
+$conexao = mysqli_connect("localhost", "root", "", "bd_webachei");
--- dashboard/empresa/Conexao.php
+++ dashboard/empresa/Conexao.php
@@ -3,7 +3,7 @@
     class Conexao{
         public $conexao;
         function __construct(){
-            $this->conexao = new PDO("mysql:host=webachei.com.br;dbname=webachei_bd", "webachei_admin", "@ZBweee6Iw?M", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $this->conexao = new PDO("mysql:host=localhost;dbname=bd_webachei", "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         }
     }
 
