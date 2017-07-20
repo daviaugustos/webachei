06bb6e4f18ca3e125881ddbdd12dbf83690227d5
Thelio <marquesthelio@hotmail.com>
2017-07-20 16:07:41 -0300

    padronização (gambiarra) diretório raiz

--- dashboard/banner/carrega-detalhe-banner.php
+++ dashboard/banner/carrega-detalhe-banner.php
@@ -1,7 +1,15 @@
 <?php 
 
     require_once("Banner.php");
-    require_once($_SERVER['DOCUMENT_ROOT'] . "/webachei/dashboard/empresa/Empresa.php");
+
+    /*Pedaço de gambiarra*/
+    $raiz = $_SERVER['DOCUMENT_ROOT'];
+    if (substr($raiz,-6) == "htdocs"){
+        $raiz = $raiz ."/webachei/";
+    }
+    /*Pedaço de gambiarra*/
+
+    require_once($raiz . "/dashboard/empresa/Empresa.php");
     $bannerObj = new Banner();
     $empresaObj = new Empresa();
 
